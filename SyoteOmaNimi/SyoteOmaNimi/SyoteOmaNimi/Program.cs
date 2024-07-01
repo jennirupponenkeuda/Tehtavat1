fd45f944fd03fa866fd57785d8c4ef488e9e50f7
jennirupponenkeuda <jenni.rupponen@edu.keuda.fi>
2024-07-01 12:07:45 +0300

    .

---
+++ SyoteOmaNimi/SyoteOmaNimi/SyoteOmaNimi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyoteOmaNimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mikä on nimesi?");
+            string nimi = Console.ReadLine();
+            Console.Write("Hei " + nimi);
+        }
+    }
+}
+
+//System.DateTime -luokan avulla määritellään aikaa päivämäärissä ja vuosissa. Sen lisäksi myös voidaan määritellä kellonaikoja.//
+//System.Math -luokan avulla voidaan määritellä erilaisia matemaattisia toimintoja (esim funktioita yms.)//
